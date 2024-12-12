4c8881449e20d55c07f6d2f3ec7568fd9040ab29
adai <trananhdai.itqnu@gmail.com>
2024-12-12 10:41:10 +0700

    Add project files.

---
+++ Test_Github/Models/SinhVien.cs
@@ -0,0 +1,16 @@
+﻿namespace Test_Github.Models
+{
+    public class SinhVien
+    {
+        private int _id;
+        private string _name;
+        private string _description;
+
+        public SinhVien(int id, string name, string description)
+        {
+            _id = id;
+            _name = name;
+            _description = description;
+        }
+    }
+}
